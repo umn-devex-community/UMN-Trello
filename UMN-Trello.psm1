d1929435cd4cbe4fbd3fc6896fa85852d35c99af
kcweeks <kylecweeks@gmail.com>
2017-12-21 12:04:50 -0600

    Commit test
    Verify internal work flow for updating modules to gitHub and PowerShell
    Gallery.

--- UMN-Trello.psm1
+++ UMN-Trello.psm1
@@ -1,5 +1,4 @@
-﻿###
-# Copyright 2017 University of Minnesota, Office of Information Technology
+﻿# Copyright 2017 University of Minnesota, Office of Information Technology
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
